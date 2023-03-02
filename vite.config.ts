888928ca0eb133f64acbea70d550fa7257bafed6
bo0tzz <git@bo0tzz.me>
2023-03-02 18:58:50 +0100

    Install sqlite-wasm

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: { target: ['es2020'] },
+	optimizeDeps: {
+		exclude: ['sqlite-wasm-esm'],
+		esbuildOptions: { target: 'es2020' }
+	}
 });
